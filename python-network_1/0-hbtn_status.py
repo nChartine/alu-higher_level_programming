27ff49af44b6efb03c52b2b077959b55d5d5b645
nChartine <n.uwase7@gmail.com>
2024-03-24 12:50:48 -0700

    scripts

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""Import urllib library."""
-
+"""__summary__
+- Write a Python script that fetches https://alu-intranet.hbtn.io/status
+- using the urllib package.
+"""
 import urllib.request
 
 
-"""fetch the url then."""
-if __name__ == "__main__":
-    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
-        html = response.read()
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode("utf-8")))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
